12ee31027b58c17730bf5e3d5e7283bc8b235e9d
Christian Russo <christian.russo8@gmail.com>
2016-06-10 20:02:06 -0300

    toma fede

--- tp2/colecciones/parser_compra.py
+++ tp2/colecciones/parser_compra.py
@@ -1,10 +1,8 @@
 import json
 from pprint import pprint
 
-with open('usuarios.json') as data_file:    
+with open(raw_input()) as data_file:    
     data = json.load(data_file)
-    print '['
-    i = 1
 for a in data:
 	print "{"
 	print '"idCompra": ' , a['idCompra'], ","
@@ -18,9 +16,4 @@ for a in data:
 	print '"PorcentajeVenta: ": ', a['porcentajeVenta']
 	print "}" ,","
 	print '"nombre": ', '"', a['nombre'], '"'
-	if i != len(data):
-		print '},'
-	else:
-		print '}'
-	i = i +1
-print ']'
+	print '}'
