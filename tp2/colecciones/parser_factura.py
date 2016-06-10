74c8664ad63aaec5216bc7417f0135d89a406602
Christian Russo <christian.russo8@gmail.com>
2016-06-10 20:59:12 -0300

    toma guachin

--- tp2/colecciones/parser_factura.py
+++ tp2/colecciones/parser_factura.py
@@ -1,19 +1,33 @@
 import json
 from pprint import pprint
 
-with open(raw_input()) as data_file:    
+with open('factura_sql.json') as data_file:    
     data = json.load(data_file)
+
+c = {}
 for a in data:
-	print "{"
-	print '"idCompra": ' , a['idCompra'], ","
-	print '"idUsuario": ', a['idUsuario'],","
-	print '"fecha": ', '"',a['fecha'],'"',","
-	print '"cantidad": ', a['cantidad'],","
-	print '"Publicacion": {'
-	print '"idPublicacion": ', a['idPublicacion'],","
-	print '"idUsuario": ', a['idUsuario'],","
-	print '"Precio": ', a['precio'],","
-	print '"PorcentajeVenta: ": ', a['porcentajeVenta']
-	print "}" ,","
-	print '"nombre": ', '"', a['nombre'], '"'
+	c.setdefault(a["idFactura"],[]).append(a)
+
+for a in c:
+	print '{'
+	print '"idFactura": ', c[a][0]['idFactura'], ","
+	print '"IdUsuario": ',c[a][0]['idUsuario'], ","
+	print '"Fecha": ', '"',c[a][0]['fecha'],'"', ","
+	total = 0
+	for x in c[a]:
+		total = total + c[a][c[a].index(x)]['totalAPagar']
+	print '"TotalAPagar": ',total, ","
+	print '"estoyPagando": ['
+	i = 1
+	for x in c[a]:
+		print "{"
+		print '"idPublicacion":', c[a][c[a].index(x)]['idPublicacion'], ","
+		print '"TipoSuscripcion":','"',c[a][c[a].index(x)]['nombre'], '"'
+		if i == len(c[a]):
+			print "}"
+		else:
+			print "},"
+		i = i+1
+	print ']'
 	print '}'
+
