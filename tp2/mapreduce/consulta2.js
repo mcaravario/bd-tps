245c3aec4f9b36a36b68652655ec04bdf116e91a
Federico Hosen <federico.hosen@gmail.com>
2016-06-15 15:45:14 -0300

    Consulta 2 OK

--- tp2/mapreduce/consulta2.js
+++ tp2/mapreduce/consulta2.js
@@ -1,10 +1,10 @@
 //La reputacion historica de cada usuario segun la calificacion.
 // Coleccion compra
 var ej2_m = function(){
-	emit(this.idUsuarioComprador, this.CalificacionDelVendedor);
-	emit(this.Publicacion.idUsuario, this.CalificacionDelComprador);
+	if(this.calificacionDelVendedor != null) emit(this.idUsuario, this.CalificacionDelVendedor);
+	if(this.CalificacionDelComprador != null) emit(this.Publicacion.idUsuario, this.CalificacionDelComprador);
 }
 
 var ej2_r = function(k, vs){
-	emit(k, Array.sum(vs) / vs.length);
+	return ( Array.sum(vs) / vs.length);
 }
