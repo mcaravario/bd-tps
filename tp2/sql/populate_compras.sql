58f88e051b666e40f6cd63502d6d75e48b94f8f0
Christian Russo <christian.russo8@gmail.com>
2016-06-15 15:40:10 -0300

    toma guido

--- tp2/sql/populate_compras.sql
+++ tp2/sql/populate_compras.sql
@@ -1,7 +1,9 @@
-select c.idCompra, c.idUsuario,c.fecha, c.cantidad, c.idPublicacion, p.precio, tp.costo, tp.porcentajeVenta, tp.nombre  
+select c.idCompra, c.idUsuario,c.fecha, c.cantidad, c.idPublicacion, p.precio, tp.costo, tp.porcentajeVenta, tp.nombre, ca.puntaje
 from compra c 
 join publicacion p
  
 on c.idPublicacion = p.idPublicacion
 join tipo_de_publicacion tp
 on p.idTipoDePublicacion = tp.idTipoPublicacion
+join calificacion ca 
+on c.idCompra = ca.idCompra
