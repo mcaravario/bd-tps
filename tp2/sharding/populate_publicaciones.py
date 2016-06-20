94ef90f569e718944f6d16bb92bc7328ff866104
Christian Russo <christian.russo8@gmail.com>
2016-06-19 23:53:45 -0300

    asd

--- tp2/sharding/populate_publicaciones.py
+++ tp2/sharding/populate_publicaciones.py
@@ -5,13 +5,14 @@ from random import randint
 
 
 foo = ['articulo', 'servicio', 'mixto']
+print 'var pubs = ['
 for x in range(1,500000):
 	print '{'
 	print '"idPublicacion":', x,","
 	print '"TipoPublicacion":', ",",random.choice(foo),","
 	print '"idUsuarioVendedor":',randint(0,8000)
-	print '}'
-
+	print '},'
+print ']'
 
 
 
