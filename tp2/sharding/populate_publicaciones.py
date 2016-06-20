f811440f24c4211f3dbf3ad844cdc86b42a6d760
Christian Russo <christian.russo8@gmail.com>
2016-06-20 00:00:25 -0300

    asd

--- tp2/sharding/populate_publicaciones.py
+++ tp2/sharding/populate_publicaciones.py
@@ -9,7 +9,7 @@ print 'var pubs = ['
 for x in range(1,500000):
 	print '{'
 	print '"idPublicacion":', x,","
-	print '"TipoPublicacion":', ",",random.choice(foo),","
+	print '"TipoPublicacion":', '"',random.choice(foo),'",'
 	print '"idUsuarioVendedor":',randint(0,8000)
 	print '},'
 print ']'
